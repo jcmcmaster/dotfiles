74e466d8a4764ec9794b7b973a4654ed977eca55
= <jmcmaster008@gmail.com>
2019-11-08 14:50:51 -0500

    ohmyzsh in specific dirs

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -27,11 +27,11 @@ else
 fi
 
 # make links
-ln -s -f ~/projects/dotfiles/common/$env/.ohmyzshconf ~/.ohmyzshconf
 ln -s -f ~/projects/dotfiles/common/$env/.tmux.conf ~/.tmux.conf
 ln -s -f ~/projects/dotfiles/common/$env/.vimrc ~/.vimrc
 ln -s -f ~/projects/dotfiles/common/$env/.zshrc ~/.zshrc
 
+ln -s -f ~/projects/dotfiles/$place/$env/.ohmyzshconf ~/.ohmyzshconf
 ln -s -f ~/projects/dotfiles/$place/$env/.aliases ~/.aliases
 ln -s -f ~/projects/dotfiles/$place/$env/.functions ~/.functions
 ln -s -f ~/projects/dotfiles/$place/$env/.gitconfig ~/.gitconfig
