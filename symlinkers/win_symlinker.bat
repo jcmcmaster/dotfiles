da3724347e3653cabf742f785ca44bf621f3c801
Jim McMaster <jim_mcmaster@medisked.com>
2020-06-10 13:41:04 -0400

    win_symlinker init

---
+++ symlinkers/win_symlinker.bat
@@ -0,0 +1,1 @@
+mklink "c:\Users\JimMcMaster\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" c:\Users\JimMcMaster\projects\dotfiles\common\win\wt\settings.json
