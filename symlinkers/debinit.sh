0ef4e509ba21124aaf3df5352ccf41aa93cff889
Jim McMaster <jim_mcmaster@medisked.com>
2020-08-05 22:48:28 -0400

    adding debinit.sh

---
+++ symlinkers/debinit.sh
@@ -0,0 +1,41 @@
+sudo apt update -y
+
+sudo apt install -y \
+ git\
+ neovim\
+ htop\
+ python3\
+ zsh\
+ tmux\
+ fortune\
+ cowsay\
+ taskwarrior\
+ nodejs\
+ curl\
+ wget\
+ python3-pip\
+ man\
+ ripgrep
+
+sudo apt update -y && sudo apt upgrade -y
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir -p ~/.config/nvim/
+
+cd && mkdir projects && cd projects
+
+git clone https://github.com/jcmcmaster/dotfiles
+
+git clone https://github.com/jcmcmaster/scripts
+
+chmod 700 ~/projects/dotfiles/symlinkers/symlink_dotfiles.sh
+
+pip3 install pynvim
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+~/projects/dotfiles/symlinkers/symlink_dotfiles.sh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
