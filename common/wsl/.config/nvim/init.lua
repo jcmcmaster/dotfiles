675af6f7d95c6f7ec71cefb0a912ee726a7c5bc0
Jim McMaster <jmcmaster@hpfc.com>
2022-01-11 14:37:08 -0500

    Recreating init.lua

--- common/wsl/.config/nvim/init.lua
+++ common/wsl/.config/nvim/init.lua
@@ -1,1 +1,8 @@
-require('main')
+require('user.lsp')
+require('user.plugins')
+require('user.theme')
+require('user.settings')
+require('user.keymap')
+require('user.treesitter')
+require('user.startup')
+
