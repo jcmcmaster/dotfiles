ff3672132c3ca0e1d29c359bcc1eb750d3b5a141
Jim McMaster <jmcmaster@hpfc.com>
2023-02-28 13:13:46 -0500

    @args instead of \

--- common/win/ps/Microsoft.PowerShell_profile.ps1
+++ common/win/ps/Microsoft.PowerShell_profile.ps1
@@ -6,43 +6,43 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/material-jcm.omp.json" | In
 
 # Aliases and alias functions
 function cdp { Set-Location C:\Users\jmcmaster\projects }
-function d { docker }
-function dc { docker-compose }
-function ga { git add }
-function gac ($msg) { git add -A && git commit -m $msg }
-function gacp ($msg) { git add -A && git commit -m $msg && git push }
-function gbl { git branch --list }
+function d { docker @args }
+function dc { docker-compose @args }
+function ga { git add @args }
+function gac { git add -A && git commit -m @args }
+function gacp { git add -A && git commit -m @args && git push }
+function gbl { git branch --list @args }
 function gap { git add -p }
 
 # `gc` is a default alias
 New-Alias -Name getc -Value "Get-Command" -Force
-function gc ($1) { git checkout $1}
+function gc { git checkout @args }
 
 function gcdd { git checkout -- . }
 
 # `gcm` is a default alias
 New-Alias -Name getm -Value "Get-Command" -Force
-function gcm ($msg) { git commit -m $msg}
+function gcm { git commit -m @args }
 
-function gcmp ($msg) { git commit -m $msg && git push}
-function gd ($1) { git diff $1 }
-function gdd ($1) { git difftool --dir-diff --no-symlinks $1}
-function gdt { git difftool }
-function gf { git fetch }
+function gcmp { git commit -m @args && git push }
+function gd { git diff @args }
+function gdd { git difftool --dir-diff --no-symlinks @args }
+function gdt { git difftool @args }
+function gf { git fetch @args }
 
 # `gl` is a default alias
 New-Alias -Name getl -Value "Get-Location" -Force
-function gl { git log }
+function gl { git log @args }
 
-function gmt { git mergetool }
+function gmt { git mergetool @args }
 
 # `gp` is a default alias
 New-Alias -Name getp -Value "Get-ItemProperty" -Force
-function gp { git pull }
+function gp { git pull @args }
 
-function gs { git status }
-function gsa { git stash apply }
-function gsl { git stash list }
-function gsp { git stash pop }
-function gtree { git log --graph --oneline --decorate }
+function gs { git status @args }
+function gsa { git stash apply @args }
+function gsl { git stash list @args }
+function gsp { git stash pop @args }
+function gtree { git log --graph --oneline --decorate @args }
 function py { python3 }
