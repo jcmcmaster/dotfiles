de9f8f678cb5a07320309a39ba11f7d936137dbc
Jim McMaster <472696@wegmans.com>
2024-02-08 12:11:28 -0500

    Telescope hidden files w/o ignored files

--- linux/.config/nvim/after/plugin/telescope.lua
+++ linux/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,26 @@
+require('telescope').setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden'
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = { 'rg', '--ignore', '--hidden', '--files', '--follow', '--glob', '!**/.git/*' }
+    }
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fF", ":Telescope find_files hidden=true no_ignore=true no_ignore_parent=true follow=true<CR>", {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fG", ":Telescope live_grep_args<CR>", {})
 vim.keymap.set("n", "<leader>fr", builtin.registers, {})
 vim.keymap.set("n", "<leader>ft", builtin.builtin, {})
