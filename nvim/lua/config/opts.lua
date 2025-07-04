b1c479fa71af5b1e4c042d52cf4e52e65ee04e87
Jim <jmcmaster008@gmail.com>
2025-07-03 23:19:52 -0400

    formatting

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -1,37 +1,37 @@
 return {
-	setup = function()
-		vim.opt.smartindent = true
+  setup = function()
+    vim.opt.smartindent = true
 
-		vim.opt.expandtab = true
-		vim.opt.smarttab = true
-		vim.opt.tabstop = 2
-		vim.opt.shiftwidth = 2
-		vim.opt.softtabstop = 2
+    vim.opt.expandtab = true
+    vim.opt.smarttab = true
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.softtabstop = 2
 
-		vim.opt.scrolloff = 8
-		vim.opt.wrap = false
+    vim.opt.scrolloff = 8
+    vim.opt.wrap = false
 
-		vim.opt.swapfile = false
-		vim.opt.backup = false
-		vim.opt.undofile = true
+    vim.opt.swapfile = false
+    vim.opt.backup = false
+    vim.opt.undofile = true
 
-		vim.opt.confirm = true
+    vim.opt.confirm = true
 
-		vim.opt.colorcolumn = '80'
-		vim.opt.signcolumn = 'yes'
-		vim.opt.termguicolors = true
+    vim.opt.colorcolumn = '80'
+    vim.opt.signcolumn = 'yes'
+    vim.opt.termguicolors = true
 
-		vim.opt.smartcase = true
+    vim.opt.smartcase = true
 
-		vim.opt.incsearch = true
+    vim.opt.incsearch = true
 
-		vim.opt.isfname:append("@-@")
+    vim.opt.isfname:append("@-@")
 
-		vim.opt.mouse = 'a'
+    vim.opt.mouse = 'a'
 
-		vim.opt.relativenumber = true
-		vim.opt.number = true
+    vim.opt.relativenumber = true
+    vim.opt.number = true
 
-		vim.opt.shell = 'pwsh.exe'
-	end
+    vim.opt.shell = 'pwsh.exe'
+  end
 }
