dd81ea4ed911c296bea476897d0e85f861a60840
Jim <jmcmaster008@gmail.com>
2025-07-04 15:15:29 -0400

    ps profile alias updates

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
 Import-Module Terminal-Icons
-Import-Module posh-git
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\material.omp.json" | Invoke-Expression
 
@@ -31,141 +30,64 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
   }
 }
 
-function Find-Project
-{
+function fp {
   $projectsDir = "$HOME\Projects"
   $projects = Get-ChildItem -Directory -Path $projectsDir | ForEach-Object FullName
   $projects += @($projectsDir)
   $target = $projects | fzf
-  if ($target)
-  {
+  if ($target) {
     Set-Location $target
   }
 }
-Set-Alias -Name fp -Value Find-Project
 
-function GitStatus
-{
-  & git status $args
-}
-Set-Alias -Name gs -Value GitStatus
+function gs { & git status $args }
 
-function GitAdd
-{
-  & git add $args
-}
-Set-Alias -Name ga -Value GitAdd
+function ga { & git add $args }
 
-function GitAddAll
-{
-  & git add -A $args
-}
-Set-Alias -Name gaa -Value GitAddAll
+function gaa { & git add -A $args }
 
-function GitBranchList
-{
-  & git branch --list $args
-}
-Set-Alias -Name gbl -Value GitBranchList
+function gbl { & git branch --list $args }
 
-function GitAddPatch
-{
-  & git add -p $args
-}
-Set-Alias -Name gap -Value GitAddPatch
+function gap { & git add -p $args }
 
-function GitCheckout
-{
-  & git checkout $args
-}
 Remove-Alias gc -Force -ErrorAction SilentlyContinue
-Set-Alias -Name gc -Value GitCheckout
+function gc { & git checkout $args }
 
-function GitCheckoutDashDashDot
-{
-  & git checkout -- .
-}
-Set-Alias -Name gcdd -Value GitCheckoutDashDashDot
+function gcdd { & git checkout -- . }
 
-function GitCommitMessage
-{
-  & git commit -m $args
-}
 Remove-Alias gcm -Force -ErrorAction SilentlyContinue
-Set-Alias -Name gcm -Value GitCommitMessage
+function gcm { & git commit -m $args }
 
-function GitDiff
-{
-  & git diff $args
-}
-Set-Alias -Name gd -Value GitDiff
+function gd { & git diff $args }
 
-function GitDiffToolDirDiff
-{
-  & git difftool --dir-diff $args
-}
-Set-Alias -Name gdd -Value GitDiffToolDirDiff
+function gdd { & git difftool --dir-diff $args }
 
-function GitDiffCached
-{
-  & git diff --cached $args
-}
-Set-Alias -Name gdc -Value GitDiffCached
+function gdc { & git diff --cached $args }
 
-function GitDiffToolDirDiffCached
-{
-  & git difftool --dir-diff --cached $args
-}
-Set-Alias -Name gddc -Value GitDiffToolDirDiffCached
+function gddc { & git difftool --dir-diff --cached $args }
 
-function GitDiffTool
-{
-  & git difftool $args
-}
-Set-Alias -Name gdt -Value GitDiffTool
+function gdt { & git difftool $args }
 
-function GitFetch
-{
-  & git fetch $args
-}
-Set-Alias -Name gf -Value GitFetch
+function gf { & git fetch $args }
 
-function GitLog
-{
-  & git log $args
-}
 Remove-Alias gl -Force -ErrorAction SilentlyContinue
-Set-Alias -Name gl -Value GitLog
+function gl { & git log $args }
 
-function GitPull
-{
-  & git pull $args
-}
 Remove-Alias gp -Force -ErrorAction SilentlyContinue
-Set-Alias -Name gp -Value GitPull
+function gp { & git pull $args }
 
-function StashList
-{
-  & git stash list
-}
-Set-Alias -Name gsl -Value StashList
+function gsl { & git stash list }
 
-function StashPop
-{
-  & git stash pop $args
-}
-Set-Alias -Name gsp -Value StashPop
+function gsp { & git stash pop $args }
 
-function Neovim
-{
-  & nvim $args
-}
-Set-Alias -Name vim -Value Neovim
+function vim { & nvim $args }
 
-function GitAddAllAndPush
-{
+function gacp {
   & git add -A
   & git commit -m "$args"
   & git push
 }
-Set-Alias -Name gacp -Value GitAddAllAndPush
+
+New-Alias g git
+
+Import-Module posh-git
