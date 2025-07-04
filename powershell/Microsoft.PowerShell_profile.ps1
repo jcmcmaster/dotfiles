72be00a0dc0f9834e0f85b6bd95c2e5741f9cc61
Jim <jmcmaster008@gmail.com>
2025-07-04 13:40:20 -0400

    powershell readline setup

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,20 @@ if ($host.Name -eq 'ConsoleHost')
   Import-Module PSReadLine -ErrorAction SilentlyContinue || Install-Module PSReadLine -Force
 }
 
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key RightArrow -Function ForwardWord
+Set-PSReadLineKeyHandler -Key LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
+Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
+Set-PSReadLineKeyHandler -Key Ctrl+k -Function ForwardDeleteLine
+
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -EditMode Vi
 
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
