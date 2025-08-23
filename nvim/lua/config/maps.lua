6875442aa394de9739c45e8a9bdc08954faff6cd
Jim <jmcmaster008@gmail.com>
2025-08-23 16:21:40 -0400

    maps.lua updates

--- nvim/lua/config/maps.lua
+++ nvim/lua/config/maps.lua
@@ -4,21 +4,23 @@ return {
     vim.g.maplocalleader = '\\'
 
     -- n
-    vim.keymap.set('n', '<C-h>', ':bprevious<CR>')
-    vim.keymap.set('n', '<C-l>', ':bnext<CR>')
+    vim.keymap.set('n', '<A-Left>', ':bprevious<CR>')
+    vim.keymap.set('n', '<A-Right>', ':bnext<CR>')
+    vim.keymap.set('n', '<A-Up>', ':tabprevious<CR>')
+    vim.keymap.set('n', '<A-Down>', ':tabnext<CR>')
 
     vim.keymap.set('n', '<C-Left>', ':wincmd h<CR>')
     vim.keymap.set('n', '<C-Down>', ':wincmd j<CR>')
     vim.keymap.set('n', '<C-Up>', ':wincmd k<CR>')
     vim.keymap.set('n', '<C-Right>', ':wincmd l<CR>')
 
-    vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>')
-    vim.keymap.set('n', '<A-Down>', ':resize -2<CR>')
-    vim.keymap.set('n', '<A-Up>', ':resize +2<CR>')
-    vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>')
+    vim.keymap.set('n', '<S-Left>', ':vertical resize -2<CR>')
+    vim.keymap.set('n', '<S-Down>', ':resize -2<CR>')
+    vim.keymap.set('n', '<S-Up>', ':resize +2<CR>')
+    vim.keymap.set('n', '<S-Right>', ':vertical resize +2<CR>')
 
-    vim.keymap.set('n', '<S-Down>', ':split<CR>')
-    vim.keymap.set('n', '<S-Right>', ':vsplit<CR>')
+    vim.keymap.set('n', '<C-S-Down>', ':split<CR>')
+    vim.keymap.set('n', '<C-S-Right>', ':vsplit<CR>')
 
     vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
