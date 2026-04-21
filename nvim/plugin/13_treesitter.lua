87bd4cae3496a08d748a20ecda99141b491be880
Jim McMaster <jmcmaster008@gmail.com>
2026-04-20 22:30:05 -0400

    Add WezTerm keybindings and update Treesitter configuration (#27)
    * feat(wezterm): keybindings
    
    * chore: init.lua from nix config
    
    * chore: nix lock
    
    * feat(treesitter): update to main

--- nvim/plugin/13_treesitter.lua
+++ nvim/plugin/13_treesitter.lua
@@ -6,85 +6,77 @@ vim.pack.add {
   'https://github.com/nvim-treesitter/nvim-treesitter-textobjects'
 }
 
-require('nvim-treesitter.config').setup({
-  ensure_installed = {
-    'bash',
-    'bicep',
-    'c',
-    'cpp',
-    'c_sharp',
-    'css',
-    'csv',
-    'diff',
-    'dockerfile',
-    'editorconfig',
-    'fsharp',
-    'git_config',
-    'git_rebase',
-    'gitattributes',
-    'gitcommit',
-    'gitignore',
-    'gleam',
-    'go',
-    'graphql',
-    'helm',
-    'html',
-    'hurl',
-    'java',
-    'javadoc',
-    'javascript',
-    'jq',
-    'json',
-    'lua',
-    'markdown',
-    'markdown_inline',
-    'powershell',
-    'python',
-    'regex',
-    'terraform',
-    'typescript',
-    'vim',
-    'vimdoc',
-    'yaml',
-  },
-  sync_install = false,
-  highlight = {
-    enable = true,
-    disable = function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['as'] = '@statement.outer',
-        ['is'] = '@statement.inner',
-        ['al'] = '@loop.outer',
-        ['il'] = '@loop.inner',
-      }
-    },
-  },
+local servers = {
+  'bash',
+  'bicep',
+  'c',
+  'cpp',
+  'c_sharp',
+  'css',
+  'csv',
+  'diff',
+  'dockerfile',
+  'editorconfig',
+  'fsharp',
+  'git_config',
+  'git_rebase',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+  'gleam',
+  'go',
+  'graphql',
+  'helm',
+  'html',
+  'hurl',
+  'java',
+  'javadoc',
+  'javascript',
+  'jq',
+  'json',
+  'lua',
+  'markdown',
+  'markdown_inline',
+  'powershell',
+  'python',
+  'regex',
+  'terraform',
+  'typescript',
+  'vim',
+  'vimdoc',
+  'yaml'
+}
+
+local treesitter = require('nvim-treesitter')
+treesitter.setup()
+treesitter.install(servers)
+
+require('nvim-treesitter-textobjects').setup()
+
+local set_select = function(maps, selection)
+  vim.keymap.set(
+    { 'x', 'o' },
+    maps,
+    function()
+      require("nvim-treesitter-textobjects.select").select_textobject(selection, "textobjects")
+    end
+  )
+end
+
+set_select('af', '@function.outer')
+set_select('if', '@function.outer')
+set_select('ac', '@class.outer')
+set_select('ic', '@class.inner')
+set_select('as', '@statement.outer')
+set_select('is', '@statement.inner')
+set_select('al', '@loop.outer')
+set_select('il', '@loop.inner')
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = servers,
+  callback = function()
+    vim.treesitter.start()
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+  end
 })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,90 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = 'rose-pine'
 config.default_prog = { '/etc/profiles/per-user/' .. os.getenv('USER') .. '/bin/fish', '-l' }
 config.font_size = 12
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.max_fps = 120
+
+config.keys = {
+  -- moving between panes
+  {
+    key = 'Home',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection('Left')
+  },
+  {
+    key = 'End',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection('Right')
+  },
+  {
+    key = 'PageUp',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection('Up')
+  },
+  {
+    key = 'PageDown',
+    mods = 'CTRL',
+    action = act.ActivatePaneDirection('Down')
+  },
+  -- resizing panes
+  {
+    key = 'Home',
+    mods = 'SHIFT',
+    action = act.AdjustPaneSize({ 'Left', 5 })
+  },
+  {
+    key = 'End',
+    mods = 'SHIFT',
+    action = act.AdjustPaneSize({ 'Right', 5 })
+  },
+  {
+    key = 'PageUp',
+    mods = 'SHIFT',
+    action = act.AdjustPaneSize({ 'Up', 5 })
+  },
+  {
+    key = 'PageDown',
+    mods = 'SHIFT',
+    action = act.AdjustPaneSize({ 'Down', 5 })
+  },
+  -- moving between tabs
+  {
+    key = 'Home',
+    mods = 'OPT',
+    action = act.ActivateTabRelative(-1)
+  },
+  {
+    key = 'End',
+    mods = 'OPT',
+    action = act.ActivateTabRelative(1)
+  },
+  -- creating panes
+  {
+    key = 'End',
+    mods = 'CTRL|SHIFT',
+    action = act.SplitHorizontal
+  },
+  {
+    key = 'PageDown',
+    mods = 'CTRL|SHIFT',
+    action = act.SplitVertical
+  },
+  -- rotating panes
+  {
+    key = 'Home',
+    mods = 'CTRL|SHIFT|OPT',
+    action = act.RotatePanes('CounterClockwise')
+  },
+  {
+    key = 'End',
+    mods = 'CTRL|SHIFT|OPT',
+    action = act.RotatePanes('Clockwise')
+  }
+}
 
 wezterm.on("gui-startup", function(cmd)
   local screen            = wezterm.gui.screens().active
