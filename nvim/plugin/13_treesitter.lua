5031218a7954fc4de8767352d62fbc5598a8a796
Jim McMaster <jmcmaster008@gmail.com>
2026-06-06 19:25:57 -0400

    feat(nvim): foldlevel 1

--- nvim/plugin/13_treesitter.lua
+++ nvim/plugin/13_treesitter.lua
@@ -78,5 +78,6 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.treesitter.start()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo[0][0].foldmethod = 'expr'
+    vim.wo[0][0].foldlevel = 1
   end
 })
