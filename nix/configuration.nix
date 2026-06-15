1222b1e32a33172507fdf39069175c6b239a0342
Jim McMaster <jmcmaster008@gmail.com>
2026-06-14 21:53:45 -0400

    feat(nix): copilot-cli installed with brew

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -9,6 +9,11 @@
   # Necessary for Determinate Nix to work correctly with nix-darwin
   nix.enable = false;
 
+  environment.systemPath = [
+    "/opt/homebrew/bin"
+    "/opt/homebrew/sbin"
+  ];
+
   programs.fish.enable = true;
 
   system.stateVersion = 4;
@@ -20,6 +25,7 @@
       upgrade = true;
     };
     casks = [
+      "copilot-cli"
       "docker-desktop"
       "spotify"
     ];
