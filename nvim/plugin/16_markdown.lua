4ec0e2d92c63afe04fe98d03ab4ebc733370f8ec
Jim McMaster <jmcmaster008@gmail.com>
2026-08-07 23:50:53 -0400

    nvim: plugin maintenance

--- nvim/plugin/16_markdown.lua
+++ nvim/plugin/16_markdown.lua
@@ -1,7 +1,6 @@
 vim.pack.add({ 'https://github.com/MeanderingProgrammer/render-markdown.nvim' })
 
 require('render-markdown').setup({
-  file_types = { 'markdown', 'codecompanion' },
   completions = {
     lsp = { enabled = true }
   }
